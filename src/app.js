ecccabd723524bdb12af6b43781103f64041265c
Debashish <debashish.panigrahi@havells.com>
2024-12-15 14:53:50 +0530

    Exploring get,post,delete api calls

--- src/app.js
+++ src/app.js
@@ -2,6 +2,19 @@ const express = require("express")
 
 const app = express()
 
+
+app.get("/user" , (req,res) =>{
+    res.send({ "firstName" : "Debashish" , "lastName" : "Panigrahi"})
+}) 
+
+app.post("/user" , (req,res) =>{
+    res.send("Data Added Successfully")
+}) 
+
+app.delete("/user" , (req,res) =>{
+    res.send("User Deleted Successfully")
+}) 
+
 app.use("/test" , (req,res) =>{
     res.send("Hello test Again")
 }) 
