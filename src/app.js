8a09e97f168683706a3f7f99e29c4ea6acbd0272
Debashish <debashish.panigrahi@havells.com>
2025-01-04 16:19:49 +0530

    encrypting password in login & signup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const connectDB = require("./config/database.js")
 const User = require("./models/user.js")
+const bcrypt = require("bcrypt")
 const app = express()
+const {validateSignUpData} = require("./utils/validation.js")
 
 const {authUser , authAdmin  } = require("./middlewares/auth.js")
 
@@ -19,12 +21,40 @@ app.use("/user/getAllData" , authUser , (req,res) => {
     res.send('User Data')
 })
 
+app.post("/login" , async(req,res) => {
+    try{
+        const {emailId , password} = req.body
 
+        const user = await User.findOne({
+            emailId : emailId
+        })
+
+        if(!user){
+            throw new Error("Email ID doesnot exist")
+        }
+
+        const isPasswordValid = await bcrypt.compare(password , user.password)
+        if(isPasswordValid){
+            res.send("Login Successful")
+        }else{
+            throw new Error("Password doesnot exist")
+        }
+
+    }catch(err){
+        res.status(400).send("Error while login" +err.message)
+    }
+})
 
 app.post("/signUp" , async (req,res) => {
-    const user = new User (req.body)
-    console.log('Welcome to body' +req.body.lastName)
-        try{
+    try{
+        validateSignUpData(req)
+        const {firstName , lastName , emailId , password} = req.body
+        const passwordHash = await bcrypt.hash(password,10)
+        console.log(passwordHash)
+        const user = new User ({
+            firstName , lastName , emailId , password : passwordHash
+        })
+       
             await user.save()
             res.send("User Added Successfully")
         }catch(err){
